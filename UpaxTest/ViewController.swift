af98eb0205e87b688bd140746a4167fd63c5d123
Aldair Carrillo <alda90dev@gmail.com>
2023-01-07 14:04:09 -0700

    First Commit

---
+++ UpaxTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UpaxTest
+//
+//  Created by Aldair Carrillo on 07/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
